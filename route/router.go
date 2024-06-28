df3f8c8078fa3d7878f8c094d2dda8b6546e6745
PuerNya <safarier@outlook.com>
2024-06-28 16:36:36 +0800

    show `DNSMode` in clash connection api

--- route/router.go
+++ route/router.go
@@ -848,6 +848,7 @@ func (r *Router) RouteConnection(ctx context.Context, conn net.Conn, metadata ad
 			Fqdn: domain,
 			Port: metadata.Destination.Port,
 		}
+		metadata.DNSMode = C.DNSModeFakeIP
 		metadata.FakeIP = true
 		r.logger.DebugContext(ctx, "found fakeip domain: ", domain)
 	}
@@ -972,6 +973,7 @@ func (r *Router) RoutePacketConnection(ctx context.Context, conn N.PacketConn, m
 			Fqdn: domain,
 			Port: metadata.Destination.Port,
 		}
+		metadata.DNSMode = C.DNSModeFakeIP
 		metadata.FakeIP = true
 		r.logger.DebugContext(ctx, "found fakeip domain: ", domain)
 	}
